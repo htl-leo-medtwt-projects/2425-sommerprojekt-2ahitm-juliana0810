8a3b92a9973ad35b39d76536235dec9a38268111
Juliana Reder <j.reder@students.htl-leonding.ac.at>
2025-03-17 12:47:58 +0100

    message progress

--- project/script.js
+++ project/script.js
@@ -17,12 +17,16 @@ function openScroll(){
         `<img id="scroll-opened" src="img/scroll-opened.png" alt="scroll-opened">
         <div class="text-container">
             <p class="typewriter delay-1">Welcome, Explorer...</p>
-            <p class="typewriter delay-2">You have ventured where few dare to tread—the cursed depths of an ancient pyramid,</p>
-            <p class="typewriter delay-3">where forgotten secrets whisper in the dark.</p>
-            <p class="typewriter delay-4">But beware... your curiosity has awakened something far worse than mere shadows.</p>
-            <p class="typewriter delay-5">The walls tremble, the air thickens, and the sands hunger for your fate.</p>
-            <p class="typewriter delay-6">Run. Solve the riddles. Escape before the last grain of time slips away.</p>
-            <p class="typewriter delay-7">This is your final expedition.</p>
-            <p class="typewriter delay-8">Make it count.</p>
+            <p class="typewriter delay-2">You have ventured where few dare to </p>
+            <p class="typewriter delay-3">tread—the cursed depths of an ancient pyramid,</p>
+            <p class="typewriter delay-4">where forgotten secrets whisper in the dark.</p>
+            <p class="typewriter delay-5">But beware... your curiosity has </p>
+            <p class="typewriter delay-6">awakened something far worse than mere shadows.</p>
+            <p class="typewriter delay-7">The walls tremble, the air thickens, </p>
+            <p class="typewriter delay-8">and the sands hunger for your fate.</p>
+            <p class="typewriter delay-9">Run. Solve the riddles.</p>
+            <p class="typewriter delay-10">Escape before the last grain of time slips away.</p>
+            <p class="typewriter delay-11">This is your final expedition.</p>
+            <p class="typewriter delay-12">Make it count.</p>
         </div>`;
 }
