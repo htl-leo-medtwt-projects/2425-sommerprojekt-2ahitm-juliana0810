9fc5b172be3ebc781e019772d9a26c5d7687aad2
Juliana Reder <j.reder@students.htl-leonding.ac.at>
2025-03-18 09:34:13 +0100

    entry page

--- project/script.js
+++ project/script.js
@@ -9,7 +9,7 @@ function openInfo(){
     infoOpen = !infoOpen;
 }
 function switchToStory(){
-    document.getElementById("startpage").style.display = "none";
+    document.getElementById("entry").style.display = "none";
     document.getElementById("storyTelling").style.display = "block";
 }
 function openScroll(){
@@ -17,11 +17,11 @@ function openScroll(){
         `<img id="scroll-opened" src="img/scroll-opened.png" alt="scroll-opened">
         <div class="text-container">
             <p class="typewriter delay-1">Welcome, Explorer...</p>
-            <p class="typewriter delay-2">You have ventured where few dare to </p>
-            <p class="typewriter delay-3">tread—the cursed depths of an ancient pyramid,</p>
+            <p class="typewriter delay-2">You have gone where few dare to go </p>
+            <p class="typewriter delay-3">—the cursed depths of an ancient pyramid,</p>
             <p class="typewriter delay-4">where forgotten secrets whisper in the dark.</p>
-            <p class="typewriter delay-5">But beware... your curiosity has </p>
-            <p class="typewriter delay-6">awakened something far worse than mere shadows.</p>
+            <p class="typewriter delay-5">But beware... your curiosity has stirred the ancient sands, </p>
+            <p class="typewriter delay-6">awakening a curse long forgotten.</p>
             <p class="typewriter delay-7">The walls tremble, the air thickens, </p>
             <p class="typewriter delay-8">and the sands hunger for your fate.</p>
             <p class="typewriter delay-9">Run. Solve the riddles.</p>
@@ -29,4 +29,8 @@ function openScroll(){
             <p class="typewriter delay-11">This is your final expedition.</p>
             <p class="typewriter delay-12">Make it count.</p>
         </div>`;
+}
+function switchToEntry(){
+    document.getElementById("startpage").style.display = "none";
+    document.getElementById("entry").style.display = "block";
 }
